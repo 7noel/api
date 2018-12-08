300389e37edc9a7b65fa4fc8af6bc1ac9b76d869
7noel <noel.logan@gmail.com>
2018-12-08 12:21:21 -0500

    sdfsdf

--- ubuntu_config/file.sql
+++ ubuntu_config/file.sql
@@ -17,7 +17,7 @@ CREATE TABLE `contributors` (
   `kilometro` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE `contributors`
   ADD PRIMARY KEY (`id`),
@@ -31,7 +31,7 @@ CREATE TABLE `sunat_exchanges` (
   `fecha` date NOT NULL,
   `compra` decimal(10,4) NOT NULL,
   `venta` decimal(10,4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE `sunat_exchanges`
   ADD PRIMARY KEY (`id`);
@@ -1922,23 +1922,23 @@ INSERT INTO `ubigeos` (`code`, `departamento`, `provincia`, `distrito`) VALUES
 ('250305','UCAYALI','PADRE ABAD ','ALEXANDER VON HUMBOLDT'),
 ('250401','UCAYALI','PURÚS','PURUS');
 
--- CREATE VIEW v_contributors AS (select c.ruc,c.razon_social,c.ubigeo,u.departamento,u.provincia,u.distrito,
--- CONCAT(CASE
---   WHEN c.nombre_via!='-' then concat(c.tipo_via,' ',c.nombre_via, CASE
---     WHEN c.numero!='-' then concat(' NRO. ',c.numero)
---     WHEN c.manzana!='-' then concat(' MZA. ',c.manzana,' LOTE ',c.lote)
---     ELSE concat(' KM. ',c.kilometro)
---     END)
---   ELSE concat('MZA. ',c.manzana,' LOTE ',c.lote)
---   END,
---   CASE WHEN c.departamento!='-' then concat(' DPTO. ',c.departamento) else concat('') END,
---   CASE WHEN c.interior!='-' then concat(' INT. ',c.interior) else concat('') END,
---   CASE WHEN c.tipo_zona!='-' then concat(case
---     WHEN c.codigo_zona='----' then concat(' ',c.tipo_zona)
---     ELSE concat(' ',c.codigo_zona,' ',c.tipo_zona) END
---     ) else concat('') END
--- ) as direccion
--- from contributors as c, ubigeos as u where c.ubigeo=u.code);
+CREATE VIEW v_contributors AS (select c.ruc,c.razon_social,c.ubigeo,u.departamento,u.provincia,u.distrito,
+CONCAT(CASE
+  WHEN c.nombre_via!='-' then concat(c.tipo_via,' ',c.nombre_via, CASE
+    WHEN c.numero!='-' then concat(' NRO. ',c.numero)
+    WHEN c.manzana!='-' then concat(' MZA. ',c.manzana,' LOTE ',c.lote)
+    ELSE concat(' KM. ',c.kilometro)
+    END)
+  ELSE concat('MZA. ',c.manzana,' LOTE ',c.lote)
+  END,
+  CASE WHEN c.departamento!='-' then concat(' DPTO. ',c.departamento) else concat('') END,
+  CASE WHEN c.interior!='-' then concat(' INT. ',c.interior) else concat('') END,
+  CASE WHEN c.tipo_zona!='-' then concat(case
+    WHEN c.codigo_zona='----' then concat(' ',c.tipo_zona)
+    ELSE concat(' ',c.codigo_zona,' ',c.tipo_zona) END
+    ) else concat('') END
+) as direccion
+from contributors as c, ubigeos as u where c.ubigeo=u.code);
 
 -- select direccion from v_contributors where ruc=10442434846;
 -- select direccion from v_contributors where ruc=20501767540;
