0d861218d257a70d96baade8a49873cde614b34c
7noel <noel.logan@gmail.com>
2019-10-21 14:15:11 -0500

    update dni

--- app/Http/Controllers/ContributorsController.php
+++ app/Http/Controllers/ContributorsController.php
@@ -36,8 +36,9 @@ class ContributorsController extends Controller {
 				$d .= ' NRO. '.$c->numero;
 			} else if ($c->manzana != '-') {
 				$d .= ' MZA. '.$c->manzana.' LOTE '.$c->lote;
-			}
-			else {
+			} else if ($c->lote != '-') {
+				$d .= ' LOTE '.$c->lote;
+			} else {
 				$d .= ' KM.'.$c->kilometro;
 			}
 			
@@ -62,15 +63,24 @@ class ContributorsController extends Controller {
 
 	public function dni($dni)
 	{
-		//dd("sds");
-		$endpoint = "http://intranet.cima.com.pe:3000/api/cimapersonal/consultas/dni/".$dni;
-		$ch = curl_init($endpoint);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$json = json_decode(curl_exec($ch));
-		$json = (isset($json->data->apaterno)) ? $json->data : null ;
-		//dd($json);
-		curl_close($ch);
-		return response()->json($json);
+		// //dd("sds");
+		// $endpoint = "http://intranet.cima.com.pe:3000/api/cimapersonal/consultas/dni/".$dni;
+		// $ch = curl_init($endpoint);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		// $json = json_decode(curl_exec($ch));
+		// $json = (isset($json->data->apaterno)) ? $json->data : null ;
+		// //dd($json);
+		// curl_close($ch);
+		// return response()->json($json);
+		$url = 'https://eldni.com/buscar-por-dni?dni='.$dni;
+		$content = file_get_contents($url);
+		$steps = explode( '<td class="text-left">' , $content );
+		if (isset($steps[3])) {
+			$data['nombres'] = explode("</td>" , $steps[1] )[0];
+			$data['apaterno'] = explode("</td>" , $steps[2] )[0];
+			$data['amaterno'] = explode("</td>" , $steps[3] )[0];
+			return response()->json($data);
+		}
+		return response()->json(null);
 	}
-
 }
